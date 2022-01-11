65b3590d2059f9f563ca7605baac160fc6b97095
Nailahbradley <bradleynailah@gmail.com>
2022-01-11 15:11:56 -0600

    added one extra day in forecast days

--- Sources/app.js
+++ Sources/app.js
@@ -39,7 +39,7 @@ function displayForecast(response) {
   let forecastHTML = `<div class="row">`;
 
   weatherForecast.forEach(function (forecastDay, index) {
-    if (index < 5) {
+    if (index < 6) {
       forecastHTML =
         forecastHTML +
         `
